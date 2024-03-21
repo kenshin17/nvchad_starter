a58f01f7a8cac0f5fb06d88e0a54ec502826f31c
rock-tran <rock.tran@manadr.com>
2024-03-21 21:47:01 +0700

    Update

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -6,20 +6,6 @@ return {
       require "configs.conform"
     end,
   },
-
-  {
-    "Asheq/close-buffers.vim",
-    cmd = "Bdelete",
-  },
-  {
-    "folke/neodev.nvim",
-    ft = { "lua" },
-    lazy = true,
-    config = function()
-      require("neodev").setup {}
-      require "configs.lspconfig-lua"
-    end,
-  },
   -- {
   --   "neovim/nvim-lspconfig",
   --   config = function()
@@ -47,4 +33,201 @@ return {
   -- 		},
   -- 	},
   -- },
+  {
+    "Asheq/close-buffers.vim",
+    cmd = "Bdelete",
+  },
+  {
+    "folke/neodev.nvim",
+    ft = { "lua" },
+    lazy = true,
+    config = function()
+      require("neodev").setup {}
+      require "configs.lspconfig-lua"
+    end,
+  },
+  {
+    "karb94/neoscroll.nvim",
+    keys = { "<C-d>", "<C-u>" },
+    config = function()
+      require("neoscroll").setup()
+    end,
+  },
+  {
+    "direnv/direnv.vim",
+    lazy = false,
+  },
+
+  {
+    "johmsalas/text-case.nvim", -- after = "ui",
+    config = function()
+      -- require("textcase").setup({})
+    end,
+    lazy = true,
+  },
+  {
+    "rmagatti/auto-session",
+    -- after = "ui",
+    config = function()
+      require("auto-session").setup {
+        log_level = "error",
+        auto_session_suppress_dirs = { "~/", "~/Downloads", "/" },
+        post_restore_cmds = { "NvimTreeToggle" },
+      }
+      require("base46").load_all_highlights()
+    end,
+    lazy = false,
+    -- cmd = "SessionRestore"
+  },
+  {
+    "tpope/vim-fugitive",
+    lazy = false,
+  },
+
+  {
+    "chaoren/vim-wordmotion",
+    lazy = false,
+  },
+
+  {
+    "smoka7/hop.nvim",
+    version = "*",
+    -- cmd = "HopWord",
+    lazy = false,
+    config = function()
+      -- you can configure Hop the way you like here; see :h hop-config
+      require("hop").setup { keys = "etovxqpdygfblzhckisuran" }
+    end,
+  },
+  {
+    "kdheepak/lazygit.nvim",
+    cmd = "LazyGit",
+    branch = "main",
+    -- after = "ui",
+  },
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim", -- required
+      "nvim-telescope/telescope.nvim", -- optional
+      "sindrets/diffview.nvim", -- optional
+    },
+    config = true,
+    lazy = false,
+  },
+  {
+    "junegunn/fzf.vim",
+    dependencies = {
+      "junegunn/fzf",
+    },
+    lazy = false,
+  },
+  {
+    "nangchan/vim-dadbod-ui",
+    dependencies = {
+      { "tpope/vim-dadbod", lazy = true },
+      { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
+    },
+    cmd = {
+      "DBUI",
+      "DBUIToggle",
+      "DBUIAddConnection",
+      "DBUIFindBuffer",
+    },
+    init = function()
+      -- Your DBUI configuration
+      vim.g.db_ui_use_nerd_fonts = 1
+      vim.g.db_ui_expand_query_results = 1
+    end,
+  },
+  {
+    "toantk238/aerial.nvim",
+    opts = {},
+    branch = "feature/more_languages",
+    -- Optional dependencies
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("aerial").setup {
+        -- optionally use on_attach to set keymaps when aerial has attached to a buffer
+        on_attach = function(bufnr)
+          -- Jump forwards/backwards with '{' and '}'
+          vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+          vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
+        end,
+        backends = { "treesitter", "lsp", "markdown", "man" },
+        -- filter_kind = false,
+        filter_kind = {
+          "Class",
+          "Constructor",
+          "Enum",
+          "Function",
+          "Interface",
+          "Module",
+          "Method",
+          "Struct",
+          "Property",
+        },
+      }
+      -- vim.keymap.set("n", "<leader>a", "<cmd>AerialNavToggle<CR>")
+      require("telescope").load_extension "aerial"
+      vim.keymap.set("n", "<leader>fc", "<cmd>:Telescope aerial<CR>")
+    end,
+    lazy = false,
+  },
+  {
+    "alexghergh/nvim-tmux-navigation",
+    lazy = false,
+    config = function()
+      require("nvim-tmux-navigation").setup {
+        disable_when_zoomed = true, -- defaults to false
+        keybindings = {
+          left = "<C-h>",
+          down = "<C-j>",
+          up = "<C-k>",
+          right = "<C-l>",
+          last_active = "<C-\\>",
+          next = "<C-Space>",
+        },
+      }
+    end,
+  },
+  {
+    "3rd/image.nvim",
+    lazy = false,
+    config = function()
+      package.path = package.path .. ";" .. vim.fn.expand "$HOME" .. "/.luarocks/share/lua/5.1/?/init.lua;"
+      package.path = package.path .. ";" .. vim.fn.expand "$HOME" .. "/.luarocks/share/lua/5.1/?.lua;"
+      require("image").setup {
+        backend = "kitty",
+        integrations = {
+          markdown = {
+            enabled = true,
+            clear_in_insert_mode = false,
+            download_remote_images = true,
+            only_render_image_at_cursor = false,
+            filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
+          },
+          neorg = {
+            enabled = true,
+            clear_in_insert_mode = false,
+            download_remote_images = true,
+            only_render_image_at_cursor = false,
+            filetypes = { "norg" },
+          },
+        },
+        max_width = nil,
+        max_height = nil,
+        max_width_window_percentage = nil,
+        max_height_window_percentage = 50,
+        window_overlap_clear_enabled = false, -- toggles images when windows are overlapped
+        window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
+        editor_only_render_when_focused = false, -- auto show/hide images when the editor gains/looses focus
+        tmux_show_only_in_active_window = false, -- auto show/hide images in the correct Tmux window (needs visual-activity off)
+        hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp" }, -- render image files as images when opened
+      }
+    end,
+  },
 }
