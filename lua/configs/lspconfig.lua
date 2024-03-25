a861c4e8c2ae535fb70384953552147aac578065
rock-tran <rock.tran@manadr.com>
2024-03-25 14:18:48 +0700

    Disable tsserver

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -39,11 +39,11 @@ for _, lsp in ipairs(servers) do
 end
 
 -- typescript
-lspconfig.tsserver.setup {
-  on_attach = on_attach,
-  on_init = on_init,
-  capabilities = capabilities,
-}
+-- lspconfig.tsserver.setup {
+--   on_attach = on_attach,
+--   on_init = on_init,
+--   capabilities = capabilities,
+-- }
 
 lspconfig.jsonls.setup {
   on_attach = on_attach,
