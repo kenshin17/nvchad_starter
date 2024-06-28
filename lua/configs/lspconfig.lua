e0c2f8b61eb63be0ad8e53debbf2aaf60cb66a81
kenshin17 <hokagevu173@gmail.com>
2024-06-28 08:00:02 +0000

    remove unuse mason

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -185,33 +185,3 @@ for command, lsp_setup in pairs(possible_lsp) do
     lsp_setup()
   end
 end
-
---local java_17_home = os.getenv("JAVA_17_HOME")
---if java_17_home then
---	local jdtls_path = os.getenv("HOME") .. "/.local/share/nvim/mason/packages/jdtls"
---
---	lspconfig.jdtls.setup({
---		on_attach = on_attach,
---		capabilities = capabilities,
---		cmd = {
---			java_17_home .. "/bin/java",
---			"-Declipse.application=org.eclipse.jdt.ls.core.id1",
---			"-Dosgi.bundles.defaultStartLevel=4",
---			"-Declipse.product=org.eclipse.jdt.ls.core.product",
---			"-Dlog.protocol=true",
---			"-Dlog.level=ALL",
---			"-Xms1g",
---			"--add-modules=ALL-SYSTEM",
---			"--add-opens",
---			"java.base/java.util=ALL-UNNAMED",
---			"--add-opens",
---			"java.base/java.lang=ALL-UNNAMED",
---			"-jar",
---			jdtls_path .. "/plugins/org.eclipse.equinox.launcher_1.6.400.v20210924-0641.jar",
---			"-configuration",
---			jdtls_path .. "/config_linux",
---			"-data",
---			os.getenv("HOME") .. "/.cache/my_jdtls",
---		},
---	})
---end
